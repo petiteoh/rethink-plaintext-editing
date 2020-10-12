f97e832a240d11834f668a9a1e23193a43373c05
petiteoh <nikkiteoh@gmail.com>
2020-10-12 15:51:50 -0700

    plain text editor

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
 
 function PlaintextEditor({ file, write }) {
-  console.log(file, write);
+  // console.log(file, write);
+
+  const [fileText, setFileText] = useState("");
+
+  function saveFile(e) {
+    e.preventDefault();
+    const savedFile = new File(
+      [fileText], file.name, {
+      type: file.type,
+      lastModified: new Date(),
+      }
+    );
+
+    write(savedFile);
+  };
+
+  function changeFile(e) {
+    setFileText(e.target.value)
+  };
+
+  useEffect(() => {
+    (async () => {
+      setFileText(await file.text());
+    })();
+  }, [file]);
+
+  let fileReader = new FileReader();
+  fileReader.readAsText(file);
+  let fileContents = fileReader.result;
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
+      <form >
+        <textarea id="" value={fileText} onChange={changeFile}>
+
+        </textarea>
+        <button onClick={saveFile}>save</button>
+      </form>
     </div>
   );
 }
